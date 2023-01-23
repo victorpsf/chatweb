7b67c7669e44773e2806fb7ce386e6b466bce4f2
victorpsf <victorpsf2@hotmail.com>
2023-01-22 21:10:29 -0300

    downloads

--- server/control/duplex.js
+++ server/control/duplex.js
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser'
 import url from 'url'
 
-export const download = function (app) {
+const queryReader = function (app) {
   app.use((req, res, next) => {
     try {
       const parsed = url.parse(req.url, true);
@@ -17,4 +17,27 @@ export const download = function (app) {
   // b, kb, mb, gb, tb ,pb
   app.use(bodyParser.json({ limit: process.env.DOWNLOAD_LENGTH }));
   app.use(bodyParser.urlencoded({ extended: false }));
+}
+
+const contentReader = function (app) {
+  app.use((req, res, next) => {
+    try {
+      const parsed = url.parse(req.url, true);
+      req.query = Object.assign({}, parsed.query);
+    }
+
+    catch (error) {
+      console.error(error);
+    }
+    next();
+  })
+
+  // b, kb, mb, gb, tb ,pb
+  app.use(bodyParser.json({ limit: process.env.DOWNLOAD_LENGTH }));
+  app.use(bodyParser.urlencoded({ extended: false }));
+}
+
+export const download = function (app) {
+  queryReader(app);
+  contentReader(app);
 }
